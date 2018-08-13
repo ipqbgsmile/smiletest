15de7934ab47426bae28cefce95e777091f8031a
smilehaiji <zhongjuezhen@126.com>
2018-08-13 15:23:51 +0800

    Update hello.php

--- hello.php
+++ hello.php
@@ -1,5 +1,5 @@
 <?php
 echo "hello!!!";
-$filename = "http://10.247.158.101:81/index.html";				//要读取的远程文件
+$filename = "http://haiji-target.default.svc.cluster.local:81/index.html";				//要读取的远程文件
 $fp = fopen($filename,"rb");	//打开文件
 echo fread($fp,1000);
