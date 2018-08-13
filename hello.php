7283b562027fdceca3a6c41abf98e97a66aeaa41
smilehaiji <zhongjuezhen@126.com>
2018-08-13 17:36:55 +0800

    Update hello.php

--- hello.php
+++ hello.php
@@ -7,3 +7,9 @@ echo fread($fp,1000);
 $filename = "http://haiji-master:81/index.html";				//要读取的远程文件
 $fp = fopen($filename,"rb");	//打开文件
 echo fread($fp,1000);
+
+
+$filename = "http://10.247.72.143:81/index.html";				//要读取的远程文件
+$fp = fopen($filename,"rb");	//打开文件
+echo fread($fp,1000);
+
