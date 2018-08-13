0d38b4f1a8d31023315b7babe0a6712607c3a6cb
smilehaiji <zhongjuezhen@126.com>
2018-08-13 14:50:55 +0800

    Update hello.php

--- hello.php
+++ hello.php
@@ -1,5 +1,5 @@
 <?php
 echo "hello!!!";
-$filename = "http://127.0.0.1/index.html";				//要读取的远程文件
+$filename = "http://haijimain.cn-north-1.huaweicse.com/index.html";				//要读取的远程文件
 $fp = fopen($filename,"rb");	//打开文件
 echo fread($fp,1000);
